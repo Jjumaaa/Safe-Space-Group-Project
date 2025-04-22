1e96acb1f050d470653ac490624bb440b34c7796
ElvyKamande <elvy.waithera@student.moringaschool.com>
2025-04-22 12:13:41 +0300

    We can now post

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Postform from './components/Postform.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Postform />
   </StrictMode>,
 )
