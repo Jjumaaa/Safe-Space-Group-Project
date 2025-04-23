c5eeeb70ff7a7ddb6d7f0862555e6612bd287964
Juma <gerald.juma@student.moringaschool.com>
2025-04-24 02:03:11 +0300

    edited about us and more blogs

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -6,25 +6,31 @@ const Aboutus = () => {
     <div>
         <header><Navbar/></header>
    
-         <p>
+        <div>
+                    <p>
+            <h1>About Us 🌟</h1>
+            
+           <h4> Welcome to Safe-Space, your go-to destination for insightful, inspiring, and engaging content! ✨ Our mission is to create a vibrant community where writers, thinkers, and readers come together to share ideas, stories, and perspectives that matter. 💡📖
 
-         About Us 🌟
-Welcome to Safe-Space, your go-to destination for insightful, inspiring, and engaging content! ✨ Our mission is to create a vibrant community where writers, thinkers, and readers come together to share ideas, stories, and perspectives that matter. 💡📖
+Whether you're looking for thought-provoking articles 📚, practical tips 🛠️, or creative storytelling 🎭, our platform offers a diverse range of topics curated to inform, entertain, and inspire. We believe in the power of words to connect people, spark conversations 🗣️, and drive positive change. 🌍</h4>
+            
+            <h1>Why Choose Us? 🤩</h1>
+            
+           <p>
+            <ol>✔ Quality Content – Handpicked and user-generated posts that resonate. ✍️❤️</ol>
+            <ol>✔ Community-Driven – A space for writers and readers to interact and grow. 👥🌱</ol>
+            <ol>✔ User-Friendly – Simple, intuitive, and designed for seamless reading and writing. 📱💻</ol>
+           </p>
 
-Whether you're looking for thought-provoking articles 📚, practical tips 🛠️, or creative storytelling 🎭, our platform offers a diverse range of topics curated to inform, entertain, and inspire. We believe in the power of words to connect people, spark conversations 🗣️, and drive positive change. 🌍
+            <h3>Join us on this journey of discovery, expression, and connection. Start reading, start writing, and be part of something meaningful! 🚀</h3>
 
-Why Choose Us? 🤩
-✔ Quality Content – Handpicked and user-generated posts that resonate. ✍️❤️
-✔ Community-Driven – A space for writers and readers to interact and grow. 👥🌱
-✔ User-Friendly – Simple, intuitive, and designed for seamless reading and writing. 📱💻
+            <h2>Happy blogging! 📢💬</h2>
 
-Join us on this journey of discovery, expression, and connection. Start reading, start writing, and be part of something meaningful! 🚀
+            <h3>The Safe-Space Team 💙</h3>
 
-Happy blogging! 📢💬
-
-— The Safe-Space Team 💙
-
-         </p>
+            </p>
+        </div>
+        
        
         </div>
   )
